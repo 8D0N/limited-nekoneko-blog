b22e96ddbf66450436f77e1b4a737f1b40ac270c
8D0N <dztm84@gmail.com>
2025-02-24 09:30:21 +0000

    add vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,6 +55,7 @@ export default function RootLayout({
             © 2024 限界ねこねこブログ All rights reserved にゃ
           </div>
         </footer>
+        <Analytics />
       </body>
     </html>
   );
